5119eb202b9967e59a27ec633d14f03b40f296d3
lmatamoros <36521721+lmatamoros@users.noreply.github.com>
2022-10-10 12:25:43 -0600

    Update index.js

--- packages/pouchdb-adapter-asyncstorage/src/index.js
+++ packages/pouchdb-adapter-asyncstorage/src/index.js
@@ -109,7 +109,7 @@ function AsyncStoragePouch (dbOpts, constuctorCallback) {
 
 AsyncStoragePouch.valid = () => {
   try {
-    return require('react-native').AsyncStorage !== null
+    return require('@react-native-async-storage/async-storage') !== null
   } catch (error) {
     return false
   }
